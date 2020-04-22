170fc2b52d8a21cff67a70d8268bd684382cb9b2
risingstar <risingstar1002@mail.ru>
2020-04-22 22:42:49 +1000

    Add Login page and Home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import { Route, Switch, Link } from "react-router-dom";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-function App() {
+export default function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            RisingStar Projects
+          </Typography>
+          <Link to="/login">
+            <Button color="inherit">Login</Button>
+          </Link>
+          <Button color="inherit">SignUp</Button>
+        </Toolbar>
+      </AppBar>
+      <Switch>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/" component={Home}></Route>
+      </Switch>
     </div>
   );
 }
-
-export default App;
